8bef7dcdaec65c50290d99570b9fc2f9a901f81b
Hiroki <hiroki11hanada@gmail.com>
2022-08-05 17:39:28 +0900

    treesitter: autoinstall

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -2,12 +2,12 @@ local configs = require("nvim-treesitter.configs")
 configs.setup {
   ensure_installed = "lua",
   sync_install = false,
+  auto_install = true,
   ignore_install = { "" }, -- List of parsers to ignore installing
   highlight = {
     enable = true, -- false will disable the whole extension
     disable = { "" }, -- list of language that will be disabled
     additional_vim_regex_highlighting = true,
-
   },
   indent = { enable = true, disable = { "yaml" } },
   autopair = {
